958f160804eba1fde7ca838875deefb23f3add94
yuhcee <egbouchenna001@gmail.com>
2025-09-24 14:17:39 +0100

    refactor(AppLayout): remove NotificationProvider and NotificationContainer for simplified layout

--- bid-to-feature-frontend/app/layouts/index.tsx
+++ bid-to-feature-frontend/app/layouts/index.tsx
@@ -1,16 +1,13 @@
 import { Navbar } from "../components/navbar";
 import Footer from "../components/footer";
 import { Outlet } from "react-router";
-import { NotificationProvider } from "../components/notification/use-notification";
-import { NotificationContainer } from "../components/notification/NotificationContainer";
 
 export default function AppLayout() {
   return (
-    <NotificationProvider>
+    <>
       <Navbar />
       <Outlet />
-      <NotificationContainer />
       <Footer />
-    </NotificationProvider>
+    </>
   );
 }
