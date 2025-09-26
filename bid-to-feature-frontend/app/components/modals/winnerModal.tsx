47b2e9c0d62ff8770a927c170bc930d203fb44a7
yuhcee <egbouchenna001@gmail.com>
2025-09-26 17:08:48 +0100

    style(frontend): Apply glassmorphism to winner modal

--- bid-to-feature-frontend/app/components/modals/winnerModal.tsx
+++ bid-to-feature-frontend/app/components/modals/winnerModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Confetti from 'react-confetti';
 import { motion } from 'framer-motion';
+import useWindowSize from '~/hooks/useWindowSize';
 
 interface WinnerModalProps {
   winner: {
@@ -11,14 +12,17 @@ interface WinnerModalProps {
 }
 
 const WinnerModal: React.FC<WinnerModalProps> = ({ winner, onClose }) => {
+  const { width, height } = useWindowSize();
+
   return (
     <motion.div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50"
       onClick={onClose}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
+      <Confetti width={width} height={height} recycle={false} />
       <motion.div
         className="bg-white rounded-lg p-8 text-center relative"
         onClick={(e) => e.stopPropagation()}
@@ -27,13 +31,14 @@ const WinnerModal: React.FC<WinnerModalProps> = ({ winner, onClose }) => {
         exit={{ scale: 0.5, opacity: 0 }}
         transition={{ type: 'spring', stiffness: 300, damping: 30 }}
       >
-        <Confetti width={400} height={300} />
         <div className="absolute -top-16 left-1/2 -translate-x-1/2 text-8xl">
           🏆
         </div>
         <h2 className="text-2xl font-bold mb-4 mt-12">Bidding Ended!</h2>
         <p className="text-lg">The winner is:</p>
-        <p className="text-xl font-bold text-yellow-500">User {winner.pubkey}</p>
+        <p className="text-xl font-bold text-yellow-500">
+          User {winner.pubkey}
+        </p>
         <p className="text-lg">with a bid of</p>
         <p className="text-xl font-bold text-yellow-500">{winner.amount} SOL</p>
       </motion.div>
